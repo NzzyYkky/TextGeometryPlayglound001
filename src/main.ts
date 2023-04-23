17315edaef20842381381252bb2c74b730647a60
NzzyYkky <62602398+NzzyYkky@users.noreply.github.com>
2023-04-23 23:45:09 +0900

    create 3D Text

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,7 @@
 import './style.scss'
 import * as THREE from 'three';
+import { TextGeometry } from 'three/addons/geometries/TextGeometry.js';
+import { FontLoader } from 'three/examples/jsm/loaders/FontLoader.js';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 
 // size
@@ -49,6 +51,32 @@ light2.shadow.mapSize.width = 4096;
 light2.shadow.mapSize.height = 4096;
 scene.add(light2)
 
+// text loading
+const fontLoader = new FontLoader();
+
+fontLoader.load('fonts/Roboto_Condensed_Regular.json', function (font) {
+  console.log(font)
+  const textGeometry = new TextGeometry('Hello three.js!', {
+    font: font,
+    size: 80,
+    height: 5,
+    curveSegments: 12,
+    bevelEnabled: true,
+    bevelThickness: 10,
+    bevelSize: 8,
+    bevelSegments: 5
+  });
+  const textMaterial = new THREE.MeshBasicMaterial({color: 0xff0000});
+  const textMesh = new THREE.Mesh(textGeometry, textMaterial);
+
+  textMesh.castShadow = true;
+  textMesh.position.y += 15;
+  textMesh.position.z -= 40;
+  textMesh.position.x = -8;
+  textMesh.rotation.y = -0.50;
+  scene.add(textMesh);
+});
+
 // animate
 function animate() { 
   renderer.render(scene, camera);
@@ -62,6 +90,20 @@ function animate() {
   light2.position.z = Math.sin(now) * 20;
   requestAnimationFrame(animate)
 }
+
+window.addEventListener('resize', () => {
+  // update size
+	size.width = window.innerWidth;
+  size.height = window.innerHeight;
+
+  // update camera
+	camera.aspect = size.width / size.height;
+	// need this method.
+  renderer.setSize(size.width, size.height);
+  renderer.setPixelRatio(window.devicePixelRatio) // ブラウザの可変対応
+	camera.updateProjectionMatrix();
+
+})
 
 document.body.appendChild(renderer.domElement);
 animate();
